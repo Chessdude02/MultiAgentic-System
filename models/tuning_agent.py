2398a8bad53331ceba09b1db52e69bc31059f687
Chessdude02 <89892754+Chessdude02@users.noreply.github.com>
2025-08-25 13:40:08 -0400

    Delete models directory

--- models/tuning_agent.py
+++
@@ -1,7 +0,0 @@
-from sklearn.model_selection import GridSearchCV
-
-class TuningAgent:
-    def tune(self, model, X, y, param_grid):
-        grid = GridSearchCV(model, param_grid, cv=3)
-        grid.fit(X, y)
-        return grid.best_estimator_
